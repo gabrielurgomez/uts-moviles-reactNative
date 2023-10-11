e66d4d7489c92c0841c104edd9e4334f73b020a2
Gabriel Uribe Gomez <info@colsysnet.com>
2023-10-10 19:46:35 -0500

    hicimos listar los productos

--- src/components/ListarProductos.jsx
+++ src/components/ListarProductos.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput, ToastAndroid, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ToastAndroid, Button, FlatList } from 'react-native';
 //import Sound from 'react-native-sound';
 
 const ListarProductosTienda = () => {
@@ -13,39 +13,58 @@ const ListarProductosTienda = () => {
     const consultarProductos = async () => {
         //ToastAndroid.show('Se consultará productos', ToastAndroid.SHORT);
         console.log('çonsultando productos...')
-        let rtaJson = await fetch('http://10.10.15.228:4000/api/productos', { method: 'GET', cache: 'no-cache', headers: { 'Content-Type': 'application/json' } })
+        let rtaJson = await fetch('http://10.10.9.101:4000/api/productos', { method: 'GET', cache: 'no-cache', headers: { 'Content-Type': 'application/json' } })
 
         console.log(rtaJson.status)
 
-        if (rtaJson.status == 200) {
-            let productosEncontrados = await rtaJson.json()
-            console.log(productosEncontrados)
-            //setProductos(productosEncontrados)
+        switch (rtaJson.status) {
+            case 200:
+                let productosEncontrados = await rtaJson.json()
+                console.log(productosEncontrados)
+                setProductos(productosEncontrados)
+                ToastAndroid.show("se consultaron los productos", ToastAndroid.SHORT)
+                break;
+
+            case 204:
+                console.log("No se encontraron productos")
+                ToastAndroid.show('No se encontraron productos', ToastAndroid.SHORT);
+                break;
+
+            case 500:
+
+                let rtaError = rtaJson.json()
+                console.log(rtaError.message)
+                ToastAndroid.show('Error en el servidor', ToastAndroid.SHORT);
+                break;
+
+            case 404:
+                let rtaError2 = rtaJson.json()
+                console.log(rtaError2.message)
+                ToastAndroid.show('Endpoint no encontrda', ToastAndroid.SHORT);
+                break;
         }
 
-        if (rtaJson.status == 204) {
-            console.log("No se encontraron productos")
-            ToastAndroid.show('No se encontraron productos', ToastAndroid.SHORT);
-        }
-
-        if (rtaJson.status == 500) {
-            let rtaError = rtaJson.json()
-            console.log(rtaError.message)
-            ToastAndroid.show('Error en el servidor', ToastAndroid.SHORT);
-        }
-
-        if (rtaJson.status == 404) {
-            let rtaError = rtaJson.json()
-            console.log(rtaError.message)
-            ToastAndroid.show('Endpoint no encontrda', ToastAndroid.SHORT);
-        }
     }
     const estilos = StyleSheet.create({ inputsTexto: { height: 40, width: 80, margin: 10, borderWidth: 1, padding: 10, borderColor: 'gray', borderWidth: 3 }, });
 
     return (
-        <View >
+        <View style={{ padding: 10 }}>
             <Button onPress={consultarProductos} title="Consultar" color="#841584" />
-            {productos.length > 0 && (<Text>Total encontrados: {productos.length}</Text>)}
+            
+            {productos.length > 0 && (
+                <>
+                    <Text>Total encontradoss: {productos.length}</Text>
+
+                    <FlatList data={productos}
+                        keyExtractor={item => item.id.toString()}
+                        renderItem={({ item }) => (
+                            <View style={{ padding: 10, borderBottomWidth: 1 }}>
+                                <Text>{item.nombre}</Text>
+                            </View>
+                        )}
+                    />
+                </>
+            )}
         </View>
     );
 };
